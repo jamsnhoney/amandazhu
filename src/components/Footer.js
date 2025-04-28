96703a619ebbe2fdaf4d69d679f26a7f1a641392
amanda <116103667+jamsnhoney@users.noreply.github.com>
2025-04-28 00:50:08 -0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,15 +7,12 @@ import coffee from "../assets/icons/coffee.png"
 import linkedin from "../assets/icons/linkedin.png"
 import mail from "../assets/icons/mail.png"
 import shrimp from "../assets/icons/shrimp.png"
-import turtle from "../assets/icons/turtle.png"
-
 
 function Footer() {
     return(
         <main className="footer-MAIN">
             <div className="footer-LEFT">
-                <p> amandazhu.ca </p>
-                <p> design & code by amanda zhu (04|2025) </p>
+                <p> design & coded by amanda zhu (april 2025) </p>
             </div>
             <div className="footer-MIDDLE">
                 <img src={smiskiii}/>
@@ -37,10 +34,6 @@ function Footer() {
 
                 <a href="https://us.jellycat.com/sensational-seafood-shrimp/" target="_blank" rel="noopener noreferrer">
                     <img src={coffee} alt="cawfee" className="clickable-image" />
-                </a>
-
-                <a href="https://us.jellycat.com/sensational-seafood-shrimp/" target="_blank" rel="noopener noreferrer">
-                    <img src={turtle} alt="tortle" className="clickable-image" />
                 </a>
 
 
